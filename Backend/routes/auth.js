6930a50e213d474245a3a19456b5344643b9f09e
MuxtorbekDev <muxtorbekravshanov9@gmail.com>
2022-05-25 08:54:48 +0500

    signup

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,12 +1,42 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
+const bcrypt = require("bcryptjs");
 
 router.get("/", (req, res) => {
   res.send("hello world");
 });
 
 router.post("/signup", (req, res) => {
-  console.log(req.body);
+  const { name, email, password } = req.body;
+  if (!email || !password || !name) {
+    res.status(422).json({ error: "PLase add all the fields" });
+  }
+
+  User.findOne({ email: email }).then((savedUser) => {
+    if (savedUser) {
+      return res
+        .status(422)
+        .json({ error: "User already exists with this email" });
+    }
+    bcrypt.hash(password, 12).then((hashedPassword) => {
+      const user = new User({
+        email,
+        name,
+        password: hashedPassword,
+      });
+
+      user
+        .save()
+        .then((user) => {
+          res.json({ msg: "added successfully" });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    });
+  });
 });
 
 module.exports = router;
